709893a70c5b85cf492d2591997f9662ab17810c
Sergey Smyshlyaev <sergey.smyshlyaev@gmail.com>
2023-04-01 17:57:17 +0200

    ChatGPT output

---
+++ content.js
@@ -0,0 +1,6 @@
+// Create a new element
+var newElement = document.createElement("div");
+newElement.textContent = "Hello, world!";
+
+// Add the element to the page
+document.body.appendChild(newElement);
